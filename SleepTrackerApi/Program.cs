d11a7ff746c401c4b97783059b5b049f7f2ce150
ionutng <ionutngp@gmail.com>
2024-06-14 17:00:12 +0300

    Allow CORS

--- SleepTrackerApi/Program.cs
+++ SleepTrackerApi/Program.cs
@@ -11,6 +11,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowCORS", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -20,6 +29,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowCORS");
 
 app.UseHttpsRedirection();
 
